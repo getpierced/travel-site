f189be7d895c22a0f2df8b331b21181d63491f6a
Robert Bailey <rob@humble.one>
2017-07-07 00:00:05 -0500

    compelte sprite stuff

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 
   browserSync.init({
     notify: false,
@@ -11,17 +11,17 @@ gulp.task('watch', function(){
     }
   });
 
-  watch('./app/index.html', function(){
-  browserSync.reload();
+  watch('./app/index.html', function() {
+    browserSync.reload();
   });
 
-  watch('./app/assets/styles/**/*.css', function(){
-  gulp.start('cssInject');
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
   });
 
 });
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
-  .pipe(browserSync.stream());
+    .pipe(browserSync.stream());
 });
